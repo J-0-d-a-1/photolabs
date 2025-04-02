097929f4650d74b0a09661141fe62fe6156755a9
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-04-02 15:25:57 -0700

    CHANGE: PhotoListItem component being modified to display a photo on the home page with using smpleData passing from App component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,24 +1,18 @@
 import "../styles/PhotoListItem.scss";
 
-const sampleDataForPhotoListItem = {
-  id: 1,
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  urls: {
-    full: "/Image-1-Full.jpeg",
-    regular: "/Image-1-Regular.jpeg",
-  },
-  user: {
-    username: "exampleuser",
-    name: "Joe Example",
-    profile: "/profile-1.jpg",
-  },
-};
+const PhotoListItem = (props) => {
+  const { username, imageSource, id, location, profile } = props;
 
-const PhotoListItem = () => {
-  /* Insert React */
+  return (
+    <>
+      <img src={imageSource} alt="photo" />
+      <img src={profile} alt="uesr's photo" />
+      <p>{username}</p>
+      <p>
+        {location.city}, {location.country}
+      </p>
+    </>
+  );
 };
 
 export default PhotoListItem;
